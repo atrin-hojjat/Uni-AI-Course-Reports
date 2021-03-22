7abf0bd7db1be7db8118f5b05686c14f36af8134
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-03-22 21:21:06 +0430

    Add save output option

--- Report 02/Problem 01 - Vertext Cover/visualizers/GraphVisualize.py
+++ Report 02/Problem 01 - Vertext Cover/visualizers/GraphVisualize.py
@@ -1,7 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
 
-def visualize_vertex_cover(graph, vertex_cover=[]):
+def visualize_vertex_cover(graph, vertex_cover=[], name="",
+        output=None):
     G = nx.Graph()
     color_map = []
     for i in range(len(graph)):
@@ -23,5 +25,12 @@ def visualize_vertex_cover(graph, vertex_cover=[]):
     pos = nx.spring_layout(G)
     nx.draw_networkx_edges(G, pos, alpha=0.2)
     nx.draw_networkx_nodes(G, pos, node_color=color_map, node_size=100)
+    if output:
+        if not os.path.exists(os.path.dirname(os.path.join("./output", output, f"{name}.jpg"))):
+            try: os.makedirs(os.path.dirname(os.path.join("./output", output, f"{name}.jpg")))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        plt.savefig(os.path.join("./output", output, f"{name}.jpg"))
     plt.show()
 
