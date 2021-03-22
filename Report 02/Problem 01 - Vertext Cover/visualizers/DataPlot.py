ec6ff2911344599f1a76080b2a5266795d8dd43a
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-03-22 23:55:02 +0430

    Final results

--- Report 02/Problem 01 - Vertext Cover/visualizers/DataPlot.py
+++ Report 02/Problem 01 - Vertext Cover/visualizers/DataPlot.py
@@ -1,15 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import os
-TTt = 0
 def plot_data(testname, x_lab, y_lab, ls, output=None):
     plt.title(testname)
     colors = ["#112233", "#66CC99", "#44BBFF", "#FC575E", "#c955f7"]
     for x in range(len(ls)):
         val = ls[x]
-        plt.plot(val['X'], val['Y'], colors[(x + TTt) % len(colors)], label=val['name'], linewidth=5)
+        plt.plot(val['X'], val['Y'], colors[x], label=val['name'],
+                linewidth=1)
+        print(x, val)
     plt.xlabel(x_lab)
     plt.ylabel(y_lab)
+    plt.legend()
     if output:
         if not os.path.exists(os.path.dirname(os.path.join("./output",
             output, f"{testname}.jpg"))):
@@ -19,6 +21,5 @@ def plot_data(testname, x_lab, y_lab, ls, output=None):
                 if exc.errno != errno.EEXIST:
                     raise
         plt.savefig(os.path.join("./output", output, f"{testname}.jpg"))
-    TTt = TTt + 1
     plt.show()
 
