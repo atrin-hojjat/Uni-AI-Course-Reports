8cc94c37a9b88ba9feb8890423a6d335fefc2f7a
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-04-01 23:17:04 +0430

    Generate new outputs

--- Report 02/Problem 01 - Vertext Cover/main.py
+++ Report 02/Problem 01 - Vertext Cover/main.py
@@ -2,7 +2,7 @@ from tests.RunSamples import Test
 from tests.TestIterations import TestByN, TestByP, TestSuccessByRandStart
 
 if __name__ == "__main__":
-    #  TestByP()
-    TestByN()
-    TestSuccessByRandStart()
+    TestByP(start=0.2, end=.3, diff=0.04, tries=10, nodes=20)
+    #  TestByN()
+    #  TestSuccessByRandStart()
     #  Test(save=True)
