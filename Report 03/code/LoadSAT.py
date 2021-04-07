3f0a5a64dbcd414c7fb75e8b2556b46179028036
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-04-08 00:01:23 +0430

    update codes

--- Report 03/code/LoadSAT.py
+++ Report 03/code/LoadSAT.py
@@ -16,24 +16,26 @@ def load_samples():
         return cnt
 
     sat = gen_sat(variables=n, p=0.2)
-    rich.print(sat, f"Variables: {n}, Clauses: {len(sat)}")
+    #  rich.print(sat, f"Variables: {n}, Clauses: {len(sat)}")
 
-    rich.print("Annealing")
-    path, steps, value = Annealing.annealing_search(n, sat)
-    rich.print(path, steps, value)
-    rich.print("Number of correct clauses: ", check_answer(sat, path))
+    for i in range(10):
+        rich.print("Annealing")
+        path, steps, value = Annealing.annealing_search(n, sat)
+        #  rich.print(path, steps, value)
+        rich.print("Number of correct clauses: ", check_answer(sat, path))
 
-    rich.print("Hill-climbing")
-    path, steps, value = HillClimbing.hill_climbing(n, sat)
-    rich.print(path, steps, value)
-    rich.print("Number of correct clauses: ", check_answer(sat, path))
+        rich.print("Hill-climbing")
+        path, steps, value = HillClimbing.hill_climbing(n, sat)
+        #  rich.print(path, steps, value)
+        rich.print("Number of correct clauses: ", check_answer(sat, path))
 
-    rich.print("Local Beam Search")
-    path, steps, value = LocalBeamSearch.local_beam_search(n, sat)
-    rich.print(path, steps, value)
-    rich.print("Number of correct clauses: ", check_answer(sat, path))
+        rich.print("Local Beam Search")
+        path, steps, value = LocalBeamSearch.local_beam_search(n, sat,
+                max_iters=1000)
+        #  rich.print(path, steps, value)
+        rich.print("Number of correct clauses: ", check_answer(sat, path))
 
-    rich.print("Genetic Algorithm")
-    path, steps, fitness = Genetic.genetic_algorithm(n, sat, max_iters=1000)
-    rich.print(path, steps, fitness)
-    rich.print("Number of correct clauses: ", check_answer(sat, path))
+        rich.print("Genetic Algorithm")
+        path, steps, fitness = Genetic.genetic_algorithm(n, sat, max_iters=1000)
+        #  rich.print(path, steps, fitness)
+        rich.print("Number of correct clauses: ", check_answer(sat, path))
--- Report 03/code/LoadTSP.py
+++ Report 03/code/LoadTSP.py
@@ -1,33 +1,33 @@
 from TSP import Genetic, Annealing, HillClimbing, LocalBeamSearch
-from visualizers.GraphVisualizers import visualize_tsp
 from generators import gen_weighted_graph, gen_sat
 import rich
 from tests import TestTSPByNodes
 
 def load_samples():
+    from visualizers.GraphVisualizers import visualize_tsp
     graph = gen_weighted_graph(nodes=10)
     rich.print(graph)
 
     rich.print("Annealing")
     path, steps, value = Annealing.annealing_search(graph)
     rich.print(path, steps, value)
-    visualize_tsp(graph, path=path, name="Simulated Annealing Search")
+    visualize_tsp(graph, path=path, name="Simulated Annealing Search", output="Sample 20 nodes")
 
     rich.print("Hill-climbing")
     path, steps, value = HillClimbing.hill_climbing(graph)
     rich.print(path, steps, value)
-    visualize_tsp(graph, path=path, name="Hill-Climbing")
+    visualize_tsp(graph, path=path, name="Hill-Climbing", output="Sample 20 nodes")
 
     rich.print("Local Beam Search")
     path, steps, value = LocalBeamSearch.local_beam_search(graph)
     rich.print(path, steps, value)
-    visualize_tsp(graph, path=path, name="Local Beam Search")
+    visualize_tsp(graph, path=path, name="Local Beam Search", output="Sample 20 nodes")
 
     rich.print("Genetic Algorithm")
     path, steps, fitness = Genetic.genetic_algorithm(graph, max_iters=1000)
     rich.print(path, steps, fitness)
-    visualize_tsp(graph, path=path, name="Genetic Algorithm")
+    visualize_tsp(graph, path=path, name="Genetic Algorithm", output="Sample 20 nodes")
 
 def run_tests():
-    TestTSPByNodes.test_tsp_by_nodes()
+    TestTSPByNodes.test_tsp_by_nodes(start=15, end=20, diff=2, output="Test TSP by N")
 
