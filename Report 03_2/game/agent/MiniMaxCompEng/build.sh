5ca7f3cd0dcc0f63ed117426e73b9bdac8487ac4
Atrin Hojjat <atrin.hojjat@gmail.com>
2021-04-15 15:51:49 +0430

    Update build command

--- Report 03_2/game/agent/MiniMaxCompEng/build.sh
+++ Report 03_2/game/agent/MiniMaxCompEng/build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-gcc -c $1.c
-gcc --shared -o $1 $1.o
+# gcc -std=c++17 -c $1.cpp
+# gcc --shared -std=c++17 -Wall -Wextra -Wshadow -O2 -o $1 $1.o
 
+echo "This method is depracated"
+echo "Please use the following command instead"
+echo
+echo "python setup.py build"
+echo
+
+exit -1
+
+
+g++ -std=c++17 --shared -Wall -Wextra -Wshadow -fPIC -o $1 $1.cpp
